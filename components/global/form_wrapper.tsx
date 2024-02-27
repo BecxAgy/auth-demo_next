433d4e5605c1b0a7774d0fd3648a9e3cf495b8d7
Rebeca Vitória de aguiar <RVD@kempetro.intranet>
2024-02-27 14:15:21 -0300

    CHORE: basic quotation form with validation and without post

---
+++ components/global/form_wrapper.tsx
@@ -0,0 +1,120 @@
+'use client'
+
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '../ui/textarea'
+import GroupConversionForm from './group-conversions'
+
+const formSchema = z.object({
+    name: z.string().min(2).max(50),
+    area: z.preprocess(a => parseInt(z.string().parse(a), 10), z.number()),
+    description: z
+        .string()
+        .min(10, {
+            message: 'A descrição da cotação deve ter no mínimo 10 caracteres',
+        })
+        .max(250),
+    factor: z.number(),
+    conversionsId: z.number().array().nonempty(),
+    //deliverablesId: z.number().array().nonempty(),
+})
+
+const FormWrapper = () => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            factor: 1,
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='max-w-md w-full flex flex-col gap-4'
+            >
+                {/* Name */}
+                <FormField
+                    control={form.control}
+                    name='name'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nome da Cotação</FormLabel>
+                            <FormControl>
+                                <Input className='lg:w-60' {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                {/* Area */}
+                <FormField
+                    control={form.control}
+                    name='area'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Área (m²)</FormLabel>
+                            <FormControl>
+                                <Input
+                                    type='number'
+                                    className='lg:w-52'
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                {/* Description */}
+                <FormField
+                    control={form.control}
+                    name='description'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Descrição</FormLabel>
+                            <FormControl>
+                                <Textarea
+                                    placeholder='Tell us a little bit about yourself'
+                                    className=''
+                                    {...field}
+                                />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                {/* Fator de Complexidade */}
+
+                {/* ConversionList */}
+                <GroupConversionForm form={form} />
+
+                {/* DeliverableList */}
+                <Button type='submit'>Submit</Button>
+            </form>
+        </Form>
+    )
+}
+
+export default FormWrapper
---
+++ components/global/group-conversions.tsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '../ui/form'
+import { Checkbox } from '../ui/checkbox'
+
+const GroupConversionForm = ({ form }) => {
+    const conversionsFormat = [
+        {
+            id: 1,
+            label: '.lgs',
+        },
+        {
+            id: 2,
+            label: '.rcp',
+        },
+        {
+            id: 3,
+            label: '.ptx',
+        },
+        {
+            id: 4,
+            label: '.e57',
+        },
+        {
+            id: 5,
+            label: '.lfd',
+        },
+        {
+            id: 6,
+            label: '.nwd',
+        },
+    ]
+    return (
+        <FormField
+            control={form.control}
+            name='conversionsId'
+            render={() => (
+                <FormItem>
+                    <div className='mb-4'>
+                        <FormLabel className='text-base'>Conversões</FormLabel>
+                        <FormDescription>
+                            Selecione as conversões de formato que deseja
+                        </FormDescription>
+                    </div>
+                    {conversionsFormat.map(item => (
+                        <FormField
+                            key={item.id}
+                            control={form.control}
+                            name='conversionsId'
+                            render={({ field }) => {
+                                return (
+                                    <FormItem
+                                        key={item.id}
+                                        className='flex flex-row items-start space-x-3 space-y-0'
+                                    >
+                                        <FormControl>
+                                            <Checkbox
+                                                checked={field.value?.includes(
+                                                    item.id,
+                                                )}
+                                                onCheckedChange={(
+                                                    checked: any,
+                                                ) => {
+                                                    const newValues =
+                                                        field.value
+                                                            ? [...field.value]
+                                                            : [] // Clonando os valores existentes ou inicializando um array vazio
+                                                    if (checked) {
+                                                        newValues.push(item.id) // Adicionando o ID ao array de valores
+                                                    } else {
+                                                        const index =
+                                                            newValues.indexOf(
+                                                                item.id,
+                                                            ) // Encontrando o índice do ID no array de valores
+                                                        if (index !== -1) {
+                                                            newValues.splice(
+                                                                index,
+                                                                1,
+                                                            ) // Removendo o ID do array de valores
+                                                        }
+                                                    }
+                                                    field.onChange(newValues) // Atualizando os valores do campo
+                                                }}
+                                            />
+                                        </FormControl>
+                                        <FormLabel className='text-sm font-normal'>
+                                            {item.label}
+                                        </FormLabel>
+                                    </FormItem>
+                                )
+                            }}
+                        />
+                    ))}
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    )
+}
+
+export default GroupConversionForm
