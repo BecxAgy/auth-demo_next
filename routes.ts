06eb2f6f01ccbba40a36850d0e36c62d9f0d1f3d
rebeca <beca.cout@gmail.com>
2024-03-21 09:16:46 -0300

    fix: error redirect

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ['/auth/login', '/auth/register']
+export const publicRoutes = ['/auth/login', '/auth/register', '/settings']
 
 /**
  * An array of routes that are used for authentication
