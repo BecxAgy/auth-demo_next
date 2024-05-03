50cfbb62c52121dae40e255815cdc9cfa7d20eb4
rebeca <beca.cout@gmail.com>
2024-05-03 09:53:17 -0300

    fix: error issuer azure ad

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,12 @@ export default {
             clientId: process.env.AZURE_AD_CLIENT_ID,
             clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
             tenantId: process.env.AZURE_AD_TENANT_ID,
+            token: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/oauth2/v2.0/token`,
+            userinfo: 'https://graph.microsoft.com/oidc/userinfo',
+            authorization: {
+                url: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/oauth2/v2.0/authorize`,
+                params: { scope: 'openid profile user.Read email' },
+            },
         }),
     ],
 } satisfies NextAuthConfig
